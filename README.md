e92c65003c6c950dbc8aac19721ed1f61c094e04
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-28 18:24:37 +0530

    [Sankar]Added: Using Loop calculate emp Wage for 20 working days

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 #### Calculate Employee Wage assuming Wage per Day is 20 and Full Day Working Hour is 8.
 ### Use Case:3
 #### Refactor the Code to write a function to get work hours
+### Use Case:4
+#### Calculating Wages for a Month assuming 20 Working Days in a month
--- empWageBuilder.js
+++ empWageBuilder.js
@@ -10,10 +10,10 @@
 }
 
 //UC 2 Switch Case
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const EMP_RATE_PER_HR = 20;
 {
-    const IS_FULL_TIME = 1;
-    const IS_PART_TIME = 2;
-    const EMP_RATE_PER_HR = 20;
     let empHrs  = 0;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     switch (empCheck) {
@@ -28,25 +28,34 @@
             break;    
     }
     let  empWage = EMP_RATE_PER_HR * empHrs;
-    console.log("UC2:- Employee Wage: " + empWage);
+    console.log("UC2:- Employee Wage: " + empWage + " WorkingHours: " + empHrs);
 }
 
 //UC 3 Functions
-{
-    const IS_FULL_TIME = 1;
-    const IS_PART_TIME = 2;
-    const EMP_RATE_PER_HR = 20;
-    function getWorkingHours(empCheck) {
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                return 8;
-            case IS_PART_TIME:
-                return 4;
-            default:
-                return 0;    
-        }
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_FULL_TIME:
+            return 8;
+        case IS_PART_TIME:
+            return 4;
+        default:
+            return 0;    
     }
+}
+{
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empWage = EMP_RATE_PER_HR * getWorkingHours(empCheck);
-    console.log("UC3:- Employee Wage: " + empWage  + " Hours: " + getWorkingHours(empCheck));
+    console.log("UC3:- Employee Wage: " + empWage  + " WorkingHours: " + getWorkingHours(empCheck));
+}
+
+//UC4 Repitive Statements
+{
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS ; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck); 
+    }
+    let empWage = EMP_RATE_PER_HR * totalEmpHrs;
+    console.log("UC4:- Employee Wage: " + empWage  + " TotalHours: " + totalEmpHrs);
 }

c1b2cfb4b76e542a1d89e9a2f659cc8157f5b9ab
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-28 18:05:31 +0530

    [Sankar]Added: Calculate Employee Wage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Welcome to Employee Wage Builder Using JS Program
 ### Use Case:1
 #### Ability to Check Employee is present or absent
+### UsedCase:2
+#### Calculate Employee Wage assuming Wage per Day is 20 and Full Day Working Hour is 8.
--- empWageBuilder.js
+++ empWageBuilder.js
@@ -1,7 +1,33 @@
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent")
-} else {
-    console.log("Employee is Present");
+//UC 1 IF ELSE Condition
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT) {
+        console.log("UC1:- Employee is Absent");
+    } else {
+        console.log("UC1:- Employee is Present");
+    }
 }
+
+//UC 2 Switch Case
+{
+    const IS_FULL_TIME = 1;
+    const IS_PART_TIME = 2;
+    const EMP_RATE_PER_HR = 20;
+    let empHrs  = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        default:
+            empHrs = 0;
+            break;    
+    }
+    let  empWage = EMP_RATE_PER_HR * empHrs;
+    console.log("UC2:- Employee Wage: " + empWage);
+}
+
